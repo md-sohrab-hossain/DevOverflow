c608fcf32126b5265f7d07427e1bd05121f4e5ad
sajalkhan <sajalhossen@yahoo.com>
2024-11-16 21:38:24 +0600

    Config tailwind and font for this application

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="text-3xl font-black text-fuchsia-500">
+    <div className="background-light850_dark100 font-space-grotesk text-3xl font-black text-fuchsia-500">
       Welcome to the world on Next.js
     </div>
   );
