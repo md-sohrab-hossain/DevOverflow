6094f6aeddbb24730aecb162be2152dddff677a2
sajalkhan <sajalhossen@yahoo.com>
2025-01-09 00:17:03 +0600

    refactor changes

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,18 +1,42 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import React from 'react';
+import { z } from 'zod';
 
 import AuthForm from '@/components/forms/AuthForm';
 import { AUTH_FORM_CONFIGS } from '@/constants/formConfig';
+import ROUTES from '@/constants/routes';
+import { toast } from '@/hooks/use-toast';
 import { signInWithCredentials } from '@/lib/actions/auth.action';
 import { SignInSchema } from '@/lib/validations';
 
 const FORM_TYPE = 'SIGN_IN';
 
 const SignIn = () => {
+  const router = useRouter();
   const { SIGN_IN } = AUTH_FORM_CONFIGS;
 
-  return <AuthForm formType={FORM_TYPE} schema={SignInSchema} formConfig={SIGN_IN} onSubmit={signInWithCredentials} />;
+  const handleSignIn = async (data: z.infer<typeof SignInSchema>) => {
+    const result = (await signInWithCredentials(data)) as ActionResponse;
+
+    if (result?.success) {
+      toast({
+        title: 'Success',
+        description: SIGN_IN.successMessage,
+      });
+
+      router.push(ROUTES.HOME);
+    } else {
+      toast({
+        title: `Error ${result?.status}`,
+        description: result?.error?.message,
+        variant: 'destructive',
+      });
+    }
+  };
+
+  return <AuthForm formType={FORM_TYPE} schema={SignInSchema} formConfig={SIGN_IN} onSubmit={handleSignIn} />;
 };
 
 export default SignIn;
--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -3,16 +3,12 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ReloadIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { useTransition } from 'react';
-import { DefaultValues, FieldValues, useForm, Path, SubmitHandler } from 'react-hook-form';
+import { DefaultValues, FieldValues, Path, useForm } from 'react-hook-form';
 import { z, ZodType } from 'zod';
 
 import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
-import { AUTH_FORM_CONFIGS, FormConfig } from '@/constants/formConfig';
-import ROUTES from '@/constants/routes';
-import { toast } from '@/hooks/use-toast';
+import { FormConfig } from '@/constants/formConfig';
 
 import { AuthFormField } from './AuthFormField';
 
@@ -20,56 +16,31 @@ interface AuthFormProps<T extends FieldValues> {
   schema: ZodType<T>;
   formType: 'SIGN_IN' | 'SIGN_UP';
   formConfig: FormConfig;
-  onSubmit: (data: T) => Promise<ActionResponse>;
+  onSubmit: (data: T) => void;
 }
 
 export default function AuthForm<T extends FieldValues>({ schema, formType, formConfig, onSubmit }: AuthFormProps<T>) {
-  const router = useRouter();
-  const [isPending, startTransition] = useTransition();
-  const { SIGN_IN, SIGN_UP } = AUTH_FORM_CONFIGS;
-
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
     defaultValues: formConfig.defaultValues as DefaultValues<T>,
   });
 
-  const buttonText = isPending
+  const buttonText = form.formState.isSubmitting
     ? `Signing ${formType === 'SIGN_IN' ? 'In' : 'Up'}...`
     : `Sign ${formType === 'SIGN_IN' ? 'In' : 'Up'}`;
 
-  const handleSubmit: SubmitHandler<T> = async data => {
-    startTransition(async () => {
-      const result = (await onSubmit(data)) as ActionResponse;
-
-      if (result?.success) {
-        toast({
-          title: 'Success',
-          description: formType === 'SIGN_IN' ? SIGN_IN.successMessage : SIGN_UP.successMessage,
-        });
-
-        router.push(ROUTES.HOME);
-      } else {
-        toast({
-          title: `Error ${result?.status}`,
-          description: result?.error?.message,
-          variant: 'destructive',
-        });
-      }
-    });
-  };
-
   return (
     <Form {...form}>
-      <form className="mt-10 space-y-6" onSubmit={form.handleSubmit(handleSubmit)}>
+      <form className="mt-10 space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
         {formConfig.fields.map(field => (
           <AuthFormField key={field.name} field={field} control={form.control} name={field.name as Path<T>} />
         ))}
 
         <Button
-          disabled={isPending}
+          disabled={form.formState.isSubmitting}
           className="primary-gradient paragraph-medium min-h-12 w-full rounded-2 px-4 py-3 font-inter !text-light-900"
         >
-          {isPending && <ReloadIcon className="mr-2 size-4 animate-spin" />}
+          {form.formState.isSubmitting && <ReloadIcon className="mr-2 size-4 animate-spin" />}
           {buttonText}
         </Button>
 
